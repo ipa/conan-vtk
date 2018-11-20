4fb25eddf5c87261b60ac6f10b1132f1b6bfabbd
Iwan Paolucci <iwan.paolucci@artorg.unibe.ch>
2018-11-20 11:48:36 +0100

    appveyor configuration

--- build.py
+++ build.py
@@ -1,25 +1,11 @@
-from conan.packager import ConanMultiPackager
-import copy
-import platform
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs = ["x86_64"])
-    builder.add_common_builds(pure_c=False)
-    filtered_builds = []
-    if platform.system() == "Linux":
-        for settings, options, env_vars, build_requires in builder.builds:
-            filtered_builds.append([settings, options])
-            new_options = copy.copy(options)
-            new_options["VTK:fPIC"] = True
-            filtered_builds.append([settings, new_options, env_vars, build_requires])
-        builder.builds = filtered_builds
-    for settings, options, env_vars, build_requires in builder.builds:
-        if settings["compiler"] == "Visual Studio":
-            if settings["compiler.runtime"] == "MT" or settings["compiler.runtime"] == "MTd":
-                # Ignore MT runtime
-                continue
-        new_options = copy.copy(options)
-        new_options["VTK:qt"] = True
-        filtered_builds.append([settings, new_options, env_vars, build_requires])
-    builder.builds = filtered_builds
-    builder.run()
+
+    builder = build_template_default.get_builder()
+
+builder.run()
